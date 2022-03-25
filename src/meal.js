41a7256a70841eb98c9431208ce78a9729534d18
Trish Fox-Collis <tfoxcollis@gmail.com>
2022-03-24 19:39:17 -0700

    Complete meal-tests

--- src/meal.js
+++ src/meal.js
@@ -1,15 +1,43 @@
-function nameMenuItem() {
+function nameMenuItem(food) {
+  return "Delicious " + food
+}
+
+function createMenuItem(name, price, type) {
+  return {
+    name: name,
+    price: price,
+    type: type
+  }
+}
+
+function addIngredients(ingredient, ingredArr) {
+  if(ingredArr.indexOf(ingredient) === -1) {
+    return ingredArr.push(ingredient)
+  }
+}
+
+function formatPrice(numPrice) {
+  return "$" + numPrice.toString()
 
 }
 
+function decreasePrice(price) {
+  return price * .9
+}
+
+function createRecipe(title, ingredients, type) {
+  return {
+    title: title,
+    ingredients: ingredients,
+    type: type
+  }
+}
 
 module.exports = {
   nameMenuItem,
-  // createMenuItem,
-  // addIngredients,
-  // formatPrice,
-  // decreasePrice,
-  // createRecipe
+  createMenuItem,
+  addIngredients,
+  formatPrice,
+  decreasePrice,
+  createRecipe
 }
-
-
